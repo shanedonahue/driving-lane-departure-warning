5e621ccb5474545001951370d70fe884570291a0
shanedonahue <70595389+shanedonahue@users.noreply.github.com>
2022-12-11 22:10:43 -0500

    update demos

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ from moviepy.editor import VideoFileClip
 
 if __name__ == "__main__":
 
-    demo = 2 # 1: image, 2 video
+    demo = 1 # 1: image, 2 video
 
     if demo == 1:
-        imagepath = 'examples/test3.jpg'
+        imagepath = 'examples/spitfire_road_pic.jpg'
         img = cv2.imread(imagepath)
         img_aug = process_frame(img)
+        cv2.imwrite("examples/spitfire_road_pic_augmented.jpg", img_aug)
 
         f, (ax1, ax2) = plt.subplots(1, 2, figsize=(25, 9))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -28,8 +29,8 @@ if __name__ == "__main__":
         plt.show()
 
     else:
-        video_output = 'examples/project_video_augmented.mp4'
-        clip1 = VideoFileClip("examples/project_video.mp4")
+        video_output = 'examples/spitfire_road_video_augmented.mp4'
+        clip1 = VideoFileClip("examples/spitfire_road_video.mp4")
 
         clip = clip1.fl_image(process_frame) #NOTE: it should be in BGR format
         clip.write_videofile(video_output, audio=False)
